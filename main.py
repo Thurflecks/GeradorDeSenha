e02fcd680212b6994f52a81f79125ee1398a2040
thurflecks <thurflecks.dev@gmail.com>
2024-04-04 11:26:45 -0300

    customtkinter

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import tkinter as Tk
-from tkinter import *
-import random
+import customtkinter
 from tkinter import messagebox
+from customtkinter import *
+import random
 
-root = Tk()
-root.title("Gerador De Senhas")
+
+root = customtkinter.CTk()
+root.title("Gerador de Senhas")
 root.geometry("400x400")
-root.maxsize(400 , 400)
+root.maxsize(400, 400)
 root.minsize(400, 400)
 root.configure(background="black")
 root.iconbitmap("key.ico")
@@ -31,51 +32,56 @@ def senha():
     numeros5 = chr(random.randint(48,52))
     
     if var1 == '4':
-        senha_criada.configure(text=(letra_maiuscula, numeros1, letra_minuscula, car_especial))
+        senha_criada.configure(text=(letra_maiuscula+numeros1+letra_minuscula+car_especial))
 
     elif var1 == '5':
-        senha_criada.configure(text=(letra_maiuscula, letra_minuscula, numeros1, car_especial, numeros2))
+        senha_criada.configure(text=(letra_maiuscula+letra_minuscula+numeros1+car_especial+numeros2))
 
     elif var1 == '6':
-        senha_criada.configure(text=(letra_maiuscula, numeros1, car_especial, numeros3, numeros5, letra_minuscula))
+        senha_criada.configure(text=(letra_maiuscula+numeros1+car_especial+numeros3+numeros5+letra_minuscula))
 
     elif var1 == '7':
-        senha_criada.configure(text=(letra_maiuscula, numeros1, numeros2, letra_minuscula, numeros3, numeros5, car_especial))
+        senha_criada.configure(text=(letra_maiuscula+numeros1+numeros2+letra_minuscula+numeros3+numeros5+car_especial))
 
     elif var1 == '8':
-        senha_criada.configure(text=(letra_maiuscula, numeros1, numeros2, letra_minuscula, numeros4, car_especial, numeros3, letra_minuscula))
+        senha_criada.configure(text=(letra_maiuscula+numeros1+numeros2+letra_minuscula+numeros4+car_especial+numeros3+letra_minuscula))
+    
+    return aviso.configure(text=(''))
 
 def copiar(): 
-    if senha_criada["text"] == "":
-        messagebox.showinfo("Aviso:", "Nada copiado")
+    if senha_criada.cget("text") == "":
+       return aviso.configure(text=('Nada Copiado'))
     else:
         root.clipboard_clear()
-        root.clipboard_append(senha_criada["text"])
+        root.clipboard_append(senha_criada.cget("text"))
         root.update()           
-        messagebox.showinfo("Aviso:", "Copiado")
+        return aviso.configure(text=('Copiado'))
+        
+opcoes = ["4", "5", "6", "7", "8"]
 
-opcoes = "4", "5", "6", "7", "8"
-
-var = StringVar(root)
+var = customtkinter.StringVar(root)
 var.set(opcoes[0]) 
 
-menu = OptionMenu(root, var, *opcoes)
-menu.place(x=320, y=80)
+menu = customtkinter.CTkOptionMenu(root, variable=var, values=opcoes)
+menu.place(x=240, y=70)
 
-ola = Label(text="Gerador de Senhas", bg="black", fg="white", font="Gabriola 20")
-ola.place(x=120, y=10)
 
-quantidade = Label(text="Escolha o tamnho da sua senha:", bg="black", fg="white", font="Gabriola 20")
+ola = customtkinter.CTkLabel(root, text="Gerador de Senhas")
+ola.place(relx=0.5, y=30, anchor="center")
+
+quantidade = customtkinter.CTkLabel(root, text="Escolha o tamanho da sua senha:")
 quantidade.place(x=20, y=70)
 
-criar = Button(text="Gerar", bg="black", fg="white", font="arial 13 ", command=senha, height=1)
-criar.place(x= 179, y=150)
+criar = customtkinter.CTkButton(root, text="Gerar", command=senha)
+criar.place(relx=0.5, y=150, anchor="center")
 
-senha_criada = Label(bg="black", fg="white", font="arial 15")
-senha_criada.place(relx=0.5, y=230, anchor="center")
+senha_criada = customtkinter.CTkLabel(root, text="")
+senha_criada.place(relx=0.5, y=215, anchor="center")
 
-copiar = Button(text="Copiar", bg="black", fg="white", font="arial 13 ", command=copiar, height=1)
-copiar.place(x= 175, y=285)
+copiar = customtkinter.CTkButton(root, text="Copiar", command=copiar)
+copiar.place(relx=0.5, y=285, anchor="center")
 
+aviso = customtkinter.CTkLabel(root, text="")
+aviso.place(relx=0.5, y=350, anchor="center")
 
 root.mainloop()
